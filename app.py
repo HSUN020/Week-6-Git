2c5f6625676d2e968d7f9f0f2804b45b0dd4cfb5
HSUN020 <144525738+HSUN020@users.noreply.github.com>
2024-02-21 17:23:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
-from flask import Flask, request,render_template
+#!/usr/bin/env python
+# coding: utf-8
 
-app= Flask (__name__)
+# In[3]:
 
-@app.route("/",methods=["GET","POST"])
+
+from textblob import TextBlob
+
+
+# In[4]:
+
+
+from flask import Flask
+
+
+# In[5]:
+
+
+app = Flask(__name__)
+
+
+# In[6]:
+
+
+from flask import render_template,request
+
+
+# In[7]:
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method =="POST":
-        rate = float(request.form.get("rate"))
-        return(render_template("index.html",result=(rate* -50.6)+90.2))
+    if request.method == "POST":
+        text = request.form.get("text")
+        print(text)
+        r1 = TextBlob(text).sentiment
+        return(render_template("index.html", result1=r1))
     else:
-        return(render_template("index.html",result="waiting for you to key in the exchange rate"))
-    
-if __name__ == "__main__":
+        return(render_template("index.html", result1="2"))
+
+
+# In[ ]:
+
+
+if __name__=="__main__":
     app.run()
